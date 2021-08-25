68b46842bdf92c6b6332285ba7c75a62328b9c1a
Lilian Umeakunne <73794416+Lilian-Chinelo@users.noreply.github.com>
2021-08-25 23:54:42 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+require('dotenv').config();
 const app = express(); 
 const https = require("https"); 
 const bodyParser = require("body-parser");
+
+
+
 
 app.use(bodyParser.urlencoded({extended: true})); 
 
@@ -11,7 +15,7 @@ app.get("/", function(req, res){
 
 app.post("/", function(req , res){ 
 const query = req.body.cityName;
-const apiKey = "";
+const apiKey = process.env.WEATHER_API_KEY;
 const units = "metric";
 const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query + "&units="+ units + "&appid=" + apiKey
 
@@ -25,8 +29,8 @@ const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query + "&unit
             const icon = weatherData.weather[0].icon;
             const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             console.log(temp);
-            res.write("<p>The weather is currently " + weatherDescription + "<p>");
-            res.write("<h3>The temperature in " + query + " is " + temp + " &deg;C</h3>");
+            res.write("<p style='font-size:18'>The weather is currently " + weatherDescription + "<p>");
+            res.write("<h2>The temperature in " + query + " is " + temp + " &deg;C</h2>");
             res.write("<img src= " + imageUrl + ">");
             res.send()//send multiple data by using the res.write as many times as needed because res.send can only be used once.
             
@@ -35,6 +39,8 @@ const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query + "&unit
         });
     });
 
-app.listen(3000, function(){
-    console.log("Server is now running at port 3000");
-});
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+      port = 3000;
+    }
+    app.listen(port);
